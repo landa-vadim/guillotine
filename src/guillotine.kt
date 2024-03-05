54ae3dde8707b279793c98caec14e2f2ab49fde8
landa-vadim <porvalrubahu@gmail.com>
2024-03-05 18:14:42 +0300

    many bug fixes, change enteredSymbol from Char to String

--- src/guillotine.kt
+++ src/guillotine.kt
@@ -1,43 +1,59 @@
 fun main() {
 
-    val words: List<String> = listOf("кукумбер", "креслок", "шкафш", "кроватьк", "столл", "диванн")
+    val words: List<String> = listOf("скатерть", "кресло", "шкаф", "кровать", "стол", "диван", "половник")
 
     val word = words.random()
     val wordLength = (word.count())
     val wordCharArray = word.toCharArray()
 
-    val list1 = mutableListOf<Char>()
-    val list2 = mutableListOf<Char>()
+    val negativeList = mutableListOf<Char>()
+    val positiveList = mutableListOf<Char>()
 
     println("Отгадайте слово:")
     println("*".repeat(wordLength))
 
-    while (list1.size < 4 && list2.size < wordLength) {
+    while (negativeList.size < 4 && positiveList.size < wordLength) {
 
         println("\nВведите букву:")
-        var readLine: Char = readln()!!.single()
-    if (list2.contains(readLine)) {
-        println("Такая буква уже есть!")
-        continue
-    }
-        if (wordCharArray.contains(readLine)) {
-                        if (list2.size < wordLength)
-                println("Верно!")
-            else {
-                println("Вы победили!")
-            }
+        val enteredSymbol: String = readln()
+        if (enteredSymbol.length != 1) {
+            println("Вы ввели не один символ!")
+            continue
         } else {
-            list1.add(readLine)
-            if (list1.size == 1) println("|\n|\n|\n|")
-            if (list1.size == 2) println("/------------\n|\n|\n|\n|")
-            if (list1.size == 3) println("/------------\n|           |\n|\n|\n|")
-            if (list1.size == 4) println("/------------\n|           |\n|           *\n|          /\\ \n|          /\\ \nВы проиграли!")
-        }
+            val letterRange: CharRange = 'а'..'я'
+            if (enteredSymbol[0] in letterRange) {
+                if (positiveList.contains(enteredSymbol[0])) {
+                    println("Такая буква уже есть!")
+                    continue
+                }
+                if (wordCharArray.contains(enteredSymbol[0])) {
+                    if (positiveList.size < wordLength-1) {
+                        println("Верно!")
+                    } else {
+                        println("Вы победили!")
+                    }
+                } else {
+                    negativeList.add(enteredSymbol[0])
+                    if (negativeList.size == 1) println("|\n|\n|\n|")
+                    if (negativeList.size == 2) println("/------------\n|\n|\n|\n|")
+                    if (negativeList.size == 3) println("/------------\n|           |\n|\n|\n|")
+                    if (negativeList.size == 4) println("/------------\n|           |\n|           *\n|          /\\ \n|          /\\ \nВы проиграли!")
+                }
 
-        for (i in wordCharArray) {
-            if (readLine == i) list2.add(readLine)
-                 if (list2.contains(i)) print(i)
-            else print("*")
+                for (i in wordCharArray) {
+                    if (enteredSymbol[0] == i) {
+                        positiveList.add(enteredSymbol[0])
+                    }
+                    if (positiveList.contains(i)) {
+                        print(i)
+                    } else {
+                        print("*")
+                    }
+                }
+            } else {
+                println("Введенный символ - не буква из кириллицы!")
+                continue
+            }
         }
     }
 }
