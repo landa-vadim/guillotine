3d07efe6b460ec749b3561ec7195a0e350b9d74a
landa-vadim <porvalrubahu@gmail.com>
2024-03-06 15:56:18 +0300

    add some func's for write if's

--- src/guillotine.kt
+++ src/guillotine.kt
@@ -5,40 +5,46 @@ fun main() {
     val word = words.random()
     val wordLength = (word.count())
     val wordCharArray = word.toCharArray()
+    val letterRange: CharRange = 'а'..'я'
 
     val negativeList = mutableListOf<Char>()
     val positiveList = mutableListOf<Char>()
+    val negativeListSize = negativeList.size
+    val positiveListSize = positiveList.size
 
     println("Отгадайте слово:")
     println("*".repeat(wordLength))
 
-    while (negativeList.size < 4 && positiveList.size < wordLength) {
-
+    while (negativeListSize < 4 && positiveListSize < wordLength) {
         println("\nВведите букву:")
         val enteredSymbol: String = readln()
         val enteredSymbolChar = enteredSymbol[0]
-        if (enteredSymbol.length != 1) {
+        if (enteredLenghtCheck(enteredSymbol)) {
             println("Вы ввели не один символ!")
             continue
         } else {
-            val letterRange: CharRange = 'а'..'я'
-            if (enteredSymbolChar in letterRange) {
-                if (positiveList.contains(enteredSymbolChar)) {
+
+            if (enteredSymbolCharRangeCheck(enteredSymbolChar, letterRange)) {
+                if (positiveListCheck(positiveList, enteredSymbolChar)) {
                     println("Такая буква уже есть!")
                     continue
                 }
-                if (wordCharArray.contains(enteredSymbolChar)) {
-                    if (positiveList.size < wordLength-1) {
+                if (rightCharCheck(wordCharArray, enteredSymbolChar)) {
+                    if (positiveListSizeCheck(positiveList, wordLength)) {
                         println("Верно!")
                     } else {
                         println("Вы победили!")
+                        return
                     }
                 } else {
                     negativeList.add(enteredSymbolChar)
                     if (negativeList.size == 1) println("|\n|\n|\n|")
                     if (negativeList.size == 2) println("/------------\n|\n|\n|\n|")
                     if (negativeList.size == 3) println("/------------\n|           |\n|\n|\n|")
-                    if (negativeList.size == 4) println("/------------\n|           |\n|           *\n|          /\\ \n|          /\\ \nВы проиграли!")
+                    if (negativeList.size == 4) {
+                        println("/------------\n|           |\n|           *\n|          /\\ \n|          /\\ \nВы проиграли!")
+                        return
+                    }
                 }
 
                 for (i in wordCharArray) {
@@ -57,4 +63,29 @@ fun main() {
             }
         }
     }
+}
+
+fun enteredLenghtCheck(enteredSymbol: String): Boolean {
+    val enteredLenghtCheck = enteredSymbol.length != 1
+    return (enteredLenghtCheck)
+    }
+
+fun enteredSymbolCharRangeCheck(enteredSymbolChar: Char, letterRange: CharRange): Boolean {
+    val enteredSymbolRangeCheck = enteredSymbolChar in letterRange
+    return enteredSymbolRangeCheck
+}
+
+fun positiveListCheck(positiveList: List<Char>, enteredSymbolChar: Char): Boolean {
+    val positiveListCheck = positiveList.contains(enteredSymbolChar)
+    return positiveListCheck
+}
+
+fun rightCharCheck(wordCharArray: CharArray, enteredSymbolChar: Char): Boolean {
+    val rightCharCheck = wordCharArray.contains(enteredSymbolChar)
+    return rightCharCheck
+}
+
+fun positiveListSizeCheck(positiveList: List<Char>, wordLength: Int): Boolean {
+    val positiveListSizeCheck = positiveList.size < wordLength-1
+    return positiveListSizeCheck
 }
